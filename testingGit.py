2981b8b7edd54793522d98c3e865a4256eae072d
GitMoey <moeyjm1998@gmail.com>
2021-03-23 20:38:03 +0000

    Added more python code

--- testingGit.py
+++ testingGit.py
@@ -3,4 +3,26 @@ import botKey
 
 key = botKey.key
 
-print("Testing for Git")
+discClient = discord.Client()
+
+# Called when the bot is ready for use
+@discClient.event
+async def onReady():
+
+    # Code replaces 0 as the client object, so it becomes discClient.user
+    print('We have logged in as {0.user}'.format(discClient))
+
+# Create another event for when bot receives a message
+# Keep in mind, we don't want the bot to respond to messages that are sent by the bot user aka us!
+@discClient.event
+async def onMessage(message):
+    # Check the author of the message, if it's by us, return
+    if message.author == discClient.user:
+        return
+
+    # Check if the message sent is a command (in this case, $hello command, which is user defined)
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello from bot!')
+
+
+discClient.run(key)
