cf8f7dc013789e709bb27bd1b3292a0f1c4b1194
GitMoey <moeyjm1998@gmail.com>
2021-03-23 21:52:19 +0000

    Added cbpro code and cbpro key to gitignore

--- testingGit.py
+++ testingGit.py
@@ -1,21 +1,28 @@
 import discord
 import botKey
+import cbpro
+import requests
+import json
+import string
 
 key = botKey.key
 
+# Create client instance
 discClient = discord.Client()
+
+# Create public coinbase pro client instance
+cbproClient = cbpro.PublicClient()
 
 # Called when the bot is ready for use
 @discClient.event
-async def onReady():
-
+async def on_ready():
     # Code replaces 0 as the client object, so it becomes discClient.user
     print('We have logged in as {0.user}'.format(discClient))
 
 # Create another event for when bot receives a message
 # Keep in mind, we don't want the bot to respond to messages that are sent by the bot user aka us!
 @discClient.event
-async def onMessage(message):
+async def on_message(message):
     # Check the author of the message, if it's by us, return
     if message.author == discClient.user:
         return
@@ -23,6 +30,19 @@ async def onMessage(message):
     # Check if the message sent is a command (in this case, $hello command, which is user defined)
     if message.content.startswith('$hello'):
         await message.channel.send('Hello from bot!')
+
+    
+
+    if message.content.startswith('$BTC-GBP'):
+        currentPrice = crypto('BTC-GBP')
+        await message.channel.send(currentPrice)
+
+# Example would be 'BTC-GBP'
+def crypto(cryptoID):
+        cryptoResponse = cbproClient.get_product_24hr_stats(str(cryptoID))
+        #jsonCryptoData = json.loads(cryptoResponse.text)
+        quote = 'Cuurrent ' + str(cryptoID) + ' price: £' + cryptoResponse['open']
+        return(quote)
 
 
 discClient.run(key)
