316c50e3e67d42a25297732c8480f2326e3d9a8d
GitMoey <moeyjm1998@gmail.com>
2021-04-30 20:26:39 +0100

    Updated code

--- testingGit.py
+++ testingGit.py
@@ -4,11 +4,24 @@ import cbpro
 import requests
 import json
 import string
+import stockmarketKey
+from twelvedata import TDClient
+import discord.ext.commands as dec
+from discord import Member
 
+smkey = stockmarketKey.SMkey
 key = botKey.key
 
-# Create client instance
-discClient = discord.Client()
+# Create bot instance
+discClient = dec.Bot(command_prefix="!")
+if __name__ == "__main__":
+    with open("botKey.key") as key:
+        token = key.read()
+
+    discClient.run(token, reconnect=True)
+
+# Create twelveData client instance
+smClient = TDClient(smkey)
 
 # Create public coinbase pro client instance
 cbproClient = cbpro.PublicClient()
@@ -16,8 +29,20 @@ cbproClient = cbpro.PublicClient()
 # Called when the bot is ready for use
 @discClient.event
 async def on_ready():
-    # Code replaces 0 as the client object, so it becomes discClient.user
-    print('We have logged in as {0.user}'.format(discClient))
+    print('We have logged in as {discClient.user}')
+
+# Called when new members join the channel
+@discClient.event
+async def on_member_join(Member):
+    await message.channel.send('Welcome to the channel, {}!'.format(Member.name))
+
+# Create an event for when the bot receives a message
+@discClient.command(
+    name="help"
+)
+async def initResponse(ctx):
+    await ctx.send("Hi, my name is {}, how may I help?".format(discClient.name))
+    
 
 # Create another event for when bot receives a message
 # Keep in mind, we don't want the bot to respond to messages that are sent by the bot user aka us!
